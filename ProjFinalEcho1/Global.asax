68cbb4c5ad638ccce6d6ff2d2807077a3d0c3ea8
Rosario <bruno.silva.rosario@gmail.com>
2019-07-09 00:53:02 +0100

    Add project files.

---
+++ ProjFinalEcho1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjFinalEcho1.MvcApplication" Language="C#" %>
---
+++ ProjFinalEcho1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjFinalEcho1.Startup))]
+namespace ProjFinalEcho1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
